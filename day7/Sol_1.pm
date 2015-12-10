44108fbe78c869aae17dfb56c8c63d7aba0b052a
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-12-09 18:13:52 -0700

    Solve recursion problem 1 day 7 :)

--- day7/Sol_1.pm
+++ day7/Sol_1.pm
@@ -3,22 +3,51 @@ my $file = "fileBig.txt";
 open(my $fh,'<:encoding(UTF-8)',$file);
 my $result =  {};
 my $circuit = {};
+my $link = {};
 
 while (my $row = <$fh>) {
   chomp $row;
-  follow_instructions($row);
+  my @first = split(" ",$row);
+  $link->{ @first[(scalar @first)-1] } = $row;
+}
+  print(find_circuit("a")."\n");
+
+sub find_circuit{
+  my $var = shift;
+  my $instruction = $link->{$var};
+  return follow_instructions($instruction);
 }
 
 
 
 sub follow_instructions{
   $row = shift;
+#  print "Following $row\n";
   my @ins = split(" ",$row);
-  if( scalar @ins == 3  ){
-    $result->{@ins[2]} = @ins[0];
+#  print (scalar @ins);
+  if( scalar @ins == 1){
+   return @ins[0];
+  }
+  elsif( $circuit->{@ins[(scalar @ins)-1]} ){
+    return  $circuit->{@ins[(scalar @ins)-1]}; 
+  }
+  elsif( scalar @ins == 3  ){
+    if (  $link->{@ins[0]} ){
+      $circuit->{@ins[0]} = follow_instructions($link->{@ins[0]});
+      return $circuit->{@ins[0]};
+    }
+    else{
+      $circuit->{@ins[2]} = @ins[0];
+      print "Assigned @ins[2] with value @ins[0]\n";
+      return $circuit->{@ins[2]};
+    }
   }
   elsif(scalar @ins == 4){
-    if(@ins[0] eq 'NOT'){
+    if( $circuit->{$ins[1]} ){
+      return $circuit->{$ins[1]};
+    }
+    else{
+      
       my $val = ""; 
       map { 
       if ($_ eq "0")
@@ -29,33 +58,34 @@ sub follow_instructions{
         { 
             $val.="0";
           } 
-        } split ("", unpack("B64", pack("n",int($ins[1]))));
+        } split ("", unpack("B64", pack("n",int(follow_instructions($link->{$ins[1]})))));
 
-      $result->{@ins[3]} = unpack( 'n', pack ('B64',$val));
-      print "Setting  @ins[3] with: ".unpack( 'n', pack ('B64',$val))."\n";
+      $circuit->{@ins[3]} = unpack( 'n', pack ('B64',$val));
+      return $circuit->{@ins[3]};
     }
   }
   else{
       if(@ins[1] eq "LSHIFT"){
-        $result->{@ins[4]} = int($result->{@ins[0]}) << int(@ins[2]);
+        $circuit->{@ins[4]} = int(follow_instructions($link->{@ins[0]}||@ins[0])) << int(@ins[2]);
+        return $circuit->{@ins[4]};
       }
       elsif(@ins[1] eq "RSHIFT"){
-       $result->{@ins[4]} = int($result->{@ins[0]}) >> int(@ins[2]);
+        $circuit->{@ins[4]} = int(follow_instructions($link->{@ins[0]}||@ins[0])) >> int($ins[2]);
+        return $circuit->{@ins[4]};
       }
       else{
         if( @ins[1] eq "AND"){
-          $result->{@ins[4]}  = int($result->{@ins[0]}) & int($result->{@ins[2]});
+          $circuit->{@ins[4]}  = int(follow_instructions($link->{@ins[0]}||@ins[0])) & int(follow_instructions($link->{@ins[2]||@ins[2]}));
+          return $circuit->{@ins[4]};
         }
         else{
-          $result->{@ins[4]}  = int($result->{@ins[0]}) | int($result->{@ins[2]});
+          $circuit->{@ins[4]}  = int(follow_instructions($link->{@ins[0]}||@ins[0])) | int(follow_instructions($link->{@ins[2]}||@ins[2]));
+          return $circuit->{@ins[4]};
         }
      }
-     print "Setting  @ins[4] with: ".$result->{@ins[4]}."\n";
+     print "Don't know what to do\n";
   }
    
 
 }
 
-print Dumper($result);
-print Dumper($result->{"a"});
-print Dumper($result->{"lx"});
