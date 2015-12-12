81dd033049404e2ec3fd984b6d39945fb0811039
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-12-11 21:53:13 -0700

    Add solution problem1 day11

---
+++ day11/Sol1.rb
@@ -0,0 +1,56 @@
+class Password
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+  
+  def get_new_password()
+    while(!check_validation(@value)) do
+      @value = @value.next    
+    end
+    return @value
+  end
+
+  def check_validation(value)
+    if value.length>8
+      puts 'Error, out of 8'
+      exit
+    end
+    return false if value.include?('i') || value.include?('o') || value.include?('l');
+    return false if !get_doubles(value)
+    return false if !get_straigh(value)
+    return true
+  end
+
+  def get_doubles(value)
+
+    count = 0
+    ant = nil
+    value.split("").each do |l|
+      if ant == l
+        count += 1
+        ant = nil
+      else
+        ant = l
+      end
+      
+    end
+    return true if count >1
+    return false
+  end
+
+  def get_straigh(value)
+    init = 'abc'
+    (0..24).each do |ind|
+      return true if value.include?(init)
+      new = ""
+      init.split("").each do |char|
+        new += char.next
+      end
+      init = new
+    end
+    return false
+  end
+end
+
