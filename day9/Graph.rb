59d23220f7fe2627c078b7501a450af9bb8e202c
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-12-11 22:07:33 -0500

    How to go into all the graph values - as a tree

--- day9/Graph.rb
+++ day9/Graph.rb
@@ -1,3 +1,5 @@
+
+require 'pp'
 class Vertice
   attr_accessor :name,:link
   def initialize(name)
@@ -15,10 +17,12 @@ class Edge
 end
 
 class Graph
-  attr_accessor :vertices, :edges
+  attr_accessor :vertices, :edges, :roads, :weights
   def initialize(fname)
     @vertices = []
     @edges = []
+    @roads = []
+    @weights = []
     f = File.open(fname,'r')
     content = f.readlines()
     content.each do |line|
@@ -37,5 +41,50 @@ class Graph
     end
     f.close()
   end
-end
+  def get_weight(sidea, sideb)
+    @edges.each do |e|
+      if(e.sidea == sidea && e.sideb == sideb )
+        return e.weight
+      elsif(e.sidea == sideb && e.sideb == sidea )
+        return e.weight
+      end
+    end
+  end
 
+  def get_neighborhs(vertice,autovisit)
+    neighbors = []
+    @edges.each do |e|
+      if(e.sidea == vertice)
+        neighbors << e.sideb unless autovisit.detect{ |te| e.sideb == te }
+      elsif(e.sideb == vertice)
+        neighbors << e.sidea unless autovisit.detect{ |te| e.sidea == te }
+      end
+    end
+    return neighbors
+  end
+
+  def go_through(initial, visited, weight)
+    pp "-------------------------"
+    autovisit = Array.new(visited)
+    autovisit << initial
+#    pp autovisit
+#    pp vertices
+    if (autovisit.size == @vertices.size )
+      pp "Encontro uno"
+      roads << autovisit
+      weights << weight
+#      pp autovisit
+#      pp weights
+      return weight.to_i;
+    else
+      temp = get_neighborhs(initial,autovisit)
+      temp.each do |to_visit|
+        if(!autovisit.detect { |av| av == to_visit })
+          pp "Visitar a #{to_visit.name}"
+          pp "With this values #{autovisit}"
+          go_through(to_visit, autovisit, (get_weight(initial,to_visit).to_i+weight.to_i))
+        end
+      end
+    end
+  end
+end
